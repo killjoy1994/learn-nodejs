23c7be826489eb7c315e78fb0ce7bbf18301aebf
Bagus Nugroho <papaobobo@gmail.com>
2022-09-04 18:35:12 +0700

    middleware commit

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
+const { urlencoded } = require("express");
 const express = require("express");
+const { json } = require("express/lib/response");
+const cors = require("cors");
 const app = express();
 const path = require("path");
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
+
+
+//custom middleware logger
+app.use(logger)
+
+// cors = Cross origin resource sharing
+const whitelist = ["https://www.yoursite.com", "http://127.0.0.1:5500", "http://localhost"];
+const corsOption = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not alloswed by CORS"))
+        }
+    },
+    optionsSuccessStatus: 200 
+}
+app.use(cors(corsOption))
+
+app.use(express.urlencoded({extended:false}))
+
+// built in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get("/index(.html)?", (req, res) => {
     // res.sendFile("./views/index.html", {root: __dirname});
@@ -43,12 +74,10 @@ const three = (req, res) => {
 app.get("/chain(.html)?", [one,two,three])
 
 //fallback no route matches
-app.get("/*", (req,res) => {
+app.use("/*", (req,res) => {
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
-
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running at port: ${PORT}`))
-
-console.log(__dirname)
