8450ac82f520ffcf909d43353506e95cee22c328
Bagus Nugroho <papaobobo@gmail.com>
2022-09-09 01:47:45 +0700

    commit for user and password authentication

--- server.js
+++ server.js
@@ -2,45 +2,46 @@ const { urlencoded } = require("express");
 const express = require("express");
 const { json } = require("express/lib/response");
 const cors = require("cors");
-const corsOptions = require("./config/corsOptions")
+const corsOptions = require("./config/corsOptions");
 const app = express();
 const path = require("path");
-const {logger} = require('./middleware/logEvents');
+const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
 
-
 //custom middleware logger
-app.use(logger)
+app.use(logger);
 
 // cors = Cross origin resource sharing
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 //built-in middlware to handle urlencoded-form data
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
 // built in middleware for json
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public")));
 
 //routes
-app.use("/", require("./routes/root"))
-app.use("/employees", require("./routes/api/employees"))
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/employees", require("./routes/api/employees"));
 
 //fallback no route matches
-app.all("*", (req,res) => {
-    res.status(404);
-    if(req.accepts(".html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"))
-    } else if(req.accepts(".json")) {
-        res.json({error: "404 not found"})
-    } else {
-        res.type("txt").send("404 not found")
-    }
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts(".html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts(".json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(PORT, console.log(`Server running at port: ${PORT}`))
+app.listen(PORT, console.log(`Server running at port: ${PORT}`));
