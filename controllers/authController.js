c9011f1565591206cd95e215eaa3c7a3028f6898
Bagus Nugroho <papaobobo@gmail.com>
2022-09-15 21:54:42 +0700

    Add JWT  authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,11 @@ const userDB = {
 
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
   if (!user || !pwd) {
@@ -20,12 +25,38 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
 
   if (match) {
-    res.json({"success": `User ${user} is logged in`})
+    //Create JWT
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "100s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //Saving refreshToken with current user
+    const otherUsers = userDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    userDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(userDB.users)
+    );
+    //httpOnly not vurnerable to javascript
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+      secure: true
+    });
+    res.json({ accessToken });
   } else {
     res.sendStatus(401); //Unauthorized
   }
-
 };
 
-
-module.exports = {handleLogin}
+module.exports = { handleLogin };
--- routes/api/employees.js
+++ routes/api/employees.js
@@ -3,14 +3,13 @@ const { get } = require("express/lib/response");
 const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
 
+router
+  .route("/")
+  .get(employeesController.getAllEmployees)
+  .post(employeesController.createNewEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee);
 
-router.route("/")
-    .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
-
-    router.route("/:id")
-        .get(employeesController.getEmployee)
+router.route("/:id").get(employeesController.getEmployee);
 
 module.exports = router;
